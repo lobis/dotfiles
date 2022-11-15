0e2a4a3fa6d2a803308a4d53131cbee839d4fe7c
Luis Obis <luis.antonio.obis@gmail.com>
2022-11-15 22:42:53 +0100

    remove source

--- install.sh
+++ install.sh
@@ -55,6 +55,4 @@ $CHEZMOI init --apply https://github.com/lobis/dotfiles.git
 TMUX_PLUGIN_MANAGER=$HOME/.tmux/plugins/tpm
 rm -rf $TMUX_PLUGIN_MANAGER
 git clone --depth=1 https://github.com/tmux-plugins/tpm $TMUX_PLUGIN_MANAGER
-tmux source ~/.tmux.conf
 sh -c $HOME/.tmux/plugins/tpm/bin/install_plugins
-tmux source ~/.tmux.conf
