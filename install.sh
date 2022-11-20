fd2ba67fbedffbeedfd139e84bd5ccb691bb1a18
Luis Obis <luis.antonio.obis@gmail.com>
2022-11-20 12:19:08 +0100

    update install of fzf

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ ZSH=$HOME/.oh-my-zsh
 rm -rf $ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-ZSH_CUSTOM="$ZSH/custom"
+ZSH_CUSTOM=$ZSH/custom
 
 # Powerlevel10k theme. The following line must be added to .zshrc: 'ZSH_THEME="powerlevel10k/powerlevel10k"''
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
@@ -23,7 +23,9 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/the
 # Install plugins. They must be added to .zshrc file
 git clone --depth=1 https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
 git clone --depth 1 https://github.com/junegunn/fzf.git $ZSH_CUSTOM/plugins/fzf
+yes | $ZSH_CUSTOM/plugins/fzf/install
 
 # This plugin must appear last in .zshrc
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
